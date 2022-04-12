793e27b6038777f6ecebabaa7dc42af80d027bbc
mohamedtamer0 <mt4452242@gmail.com>
2022-04-12 18:17:12 +0200

    update

---
+++ app/src/main/java/com/example/mvvmtemplate/di/AppModule.kt
@@ -0,0 +1,90 @@
+package com.example.mvvmtemplate.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import androidx.room.Room
+import com.example.mvvmtemplate.data.local.AppDatabase
+import com.example.mvvmtemplate.data.remote.webservice.AuthInterceptor
+import com.example.mvvmtemplate.data.remote.webservice.WebService
+import com.example.mvvmtemplate.util.Constant.BASE_URL
+import com.example.mvvmtemplate.util.Constant.DATA_STORE_NAME
+import com.example.mvvmtemplate.util.Constant.ROOM_DATA_BASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.ThreadFactory
+import java.util.concurrent.TimeUnit
+
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create {
+            context.preferencesDataStoreFile(DATA_STORE_NAME)
+        }
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideBasicAuth() = AuthInterceptor()
+
+
+    @Provides
+    @Singleton
+    fun provideOkHttp(
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): okhttp3.Call.Factory {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .callTimeout(600, TimeUnit.SECONDS)
+            .readTimeout(600, TimeUnit.SECONDS)
+            .connectTimeout(10000, TimeUnit.SECONDS)
+            .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideWebService(
+        callFactory: okhttp3.Call.Factory
+    ): WebService = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .callFactory(callFactory)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(WebService::class.java)
+
+
+    @Provides
+    @Singleton
+    fun provideStockDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(context, AppDatabase::class.java, ROOM_DATA_BASE_NAME)
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+}
